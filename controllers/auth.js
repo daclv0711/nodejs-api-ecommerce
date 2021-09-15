f8a6b5e7ddee7dafc586d08b8529bbc5703b12e2
daclv0711 <daclv0711@gmail.com>
2021-09-15 22:20:12 +0700

    updates

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,6 +41,9 @@ export const register = async (req, res, next) => {
                     existed: true
                 })
             } else {
+res.status(statusHTTP.SUCCESS).json({
+                    existed: false
+                })
 		if(password?.match(/^(?=.*?[a-z])(?=.*?[0-9]).{8,32}$/g)
         && username?.match(/^[a-zA-Z!@#\$%\^\&*\)\(+=._-]{2,}$/g)){
                 const newUser = new Auth({
